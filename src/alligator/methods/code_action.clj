da4652568fa19914561c6962865b6539ee86c6db
junyi.hou <junyi.yi.hou@gmail.com>
2026-02-05 22:54:34 -0800

    implement textDocument/codeAction

---
+++ src/alligator/methods/code_action.clj
@@ -0,0 +1,66 @@
+(ns alligator.methods.code-action
+  (:require [alligator.methods :as methods]
+            [clojure.core.async :as async]
+            [alligator.multiplexer :as mux]))
+
+(defn ^:private interject-code-action-source
+  "When applying the selected code action, client may need to send it back to the server
+   to resolve it. In this case, we need to know which server is responsible for the sent
+   back code action and relay the code action result to the correct server.
+
+   To do so, we interject a custom identifier to the `:data` field of the
+   codeActionItem. For the legacy command codeActions, use the `execute-command-provider`
+   in the server initialize response to identify the server to send the subsequent
+   execute-command request."
+  [result server-name]
+  (letfn [(interject [item]
+            (if (:kind item)
+              ;; codeActionItem (has :kind field)
+              (assoc-in item [:data :alligator-source] server-name)
+              ;; fallback - does nothing
+              item))]
+    (->> result
+         (map interject)
+         vec)))
+
+(defmethod methods/process-server-message "textDocument/codeAction"
+  [_ code-action-chan output-chan]
+  (async/go-loop [inflight {}]
+    (when-let [msg (async/<! code-action-chan)]
+      (let [{:keys [message] server-name :from} msg
+            id (:id message)
+            result (interject-code-action-source (:result message) server-name)
+            entry (or (get inflight id)
+                      {:results {}
+                       :expected-count (count (mux/server-accept-method "textDocument/codeAction"))})
+            new-results (assoc (:results entry) server-name result)]
+        (if (>= (count new-results) (:expected-count entry))
+          (do
+            (async/>! output-chan
+                      {:jsonrpc "2.0"
+                       :id id
+                       :result (vec (mapcat identity (vals new-results)))})
+            (recur (dissoc inflight id)))
+          (recur (assoc inflight id (assoc entry :results new-results))))))))
+
+(defmethod methods/process-client-message "codeAction/resolve"
+  [_ resolve-chan]
+  (async/go-loop []
+    (when-let [message (async/<! resolve-chan)]
+      (let [params (:params message)
+            server-name (get-in params [:data :alligator-source])]
+
+        (if-let [server (some #(when (= (:name %) server-name) %) @mux/enabled-servers)]
+          (async/>!
+           (:stdin server)
+           ;; remove the added :alligator-source key
+           (assoc-in message [:params :data] (dissoc (get params :data) :alligator-source)))
+
+          ;; if no server-name is found or no running server with the name, return an error
+          (async/>! mux/server-output {:jsonrpc "2.0"
+                                       :id (:id message)
+                                       :error {:code -32000
+                                               :message "Could not find the right server to relay the codeAction/Resolve request"
+                                               :data (:data params)}}))))
+
+    (recur)))
---
+++ test/alligator/methods/code_action_test.clj
@@ -0,0 +1,80 @@
+(ns alligator.methods.code-action-test
+  (:require
+   [alligator.methods :as methods]
+   [alligator.methods.code-action :as code-action]
+   [alligator.methods.execute-command :as exec-commands]
+   [alligator.multiplexer :as mux]
+   [clojure.core.async :as async]
+   [clojure.test :refer [deftest testing is use-fixtures]]))
+
+(defn reset-state [f]
+  (reset! mux/enabled-servers [])
+  (reset! exec-commands/server-commands-map {})
+  (f))
+
+(use-fixtures :each reset-state)
+
+(methods/load-handlers!)
+
+(deftest test-interject-code-action-source
+  (testing "Add origingating server to the :data field"
+    (let [server-name "server-1"
+          result [;; no :data field
+                  {:title "Foo" :kind "quickfix" :edit "some-file"}
+                  ;; has :data field
+                  {:title "Bar" :kind "refactor.inline" :command {:title "bar command" :command "bar"} :data {:key "value"}}
+                  ;; raw command
+                  {:title "Baz" :command "baz" :arguments ["1" "2"]}]]
+      (is (= (#'code-action/interject-code-action-source result server-name)
+             [{:title "Foo" :kind "quickfix" :edit "some-file" :data {:alligator-source server-name}}
+              {:title "Bar" :kind "refactor.inline" :command {:title "bar command" :command "bar"} :data {:key "value" :alligator-source server-name}}
+              {:title "Baz" :command "baz" :arguments ["1" "2"]}])))))
+
+(deftest test-process-server-message-code-action
+  (testing "Merge code action response using conj[oin]"
+    (let [in-chan (async/chan 10)
+          out-chan (async/chan 10)
+          server1 {:name "s1" :command "c1" :is-default true :capabilities nil}
+          server2 {:name "s2" :command "c2" :is-default false :capabilities [:code-action-provider]}]
+      (reset! mux/enabled-servers [server1 server2])
+
+      ;; open code action channel
+      (methods/process-server-message "textDocument/codeAction" in-chan out-chan)
+
+      (async/go
+        (async/>! in-chan {:from "s1"
+                           :message {:jsonrpc "2.0"
+                                     :id 100
+                                     :result [{:title "Foo" :kind "quickfix" :edit "some-file"}
+                                              {:title "Baz" :command "baz" :arguments ["1" "2"]}]}})
+        (async/>! in-chan {:from "s2"
+                           :message {:jsonrpc "2.0"
+                                     :id 100
+                                     :result [{:title "Bar" :kind "refactor.inline" :command {:title "bar command" :command "bar"} :data {:key "value"}}]}})
+        (let [response (async/<!! out-chan)]
+          (is (= 100 (:id response)))
+          (is (= (set (:result response))
+                 (set [{:title "Foo" :kind "quickfix" :edit "some-file" :data {:alligator-source "s1"}}
+                       {:title "Bar" :kind "refactor.inline" :command {:title "bar command" :command "bar"} :data {:key "value" :alligator-source "s2"}}
+                       {:title "Baz" :command "baz" :arguments ["1" "2"]}]))))))))
+
+;; (deftest test-process-client-message-resolve
+;;   (testing ""))
+(deftest test-process-client-message-resolve
+  (testing "Relay codeAction/resolve request to the correct server"
+    (let [server1-in-chan (async/chan 10)
+          client-out-chan (async/chan 10)
+          server1 {:name "s1" :command "c1" :stdin server1-in-chan :is-default true :capabilities nil}
+          server2 {:name "s2" :command "c2" :stdin (async/chan 1) :is-default false :capabilities [:code-action-provider]}]
+      (reset! mux/enabled-servers [server1 server2])
+
+      (methods/process-client-message "codeAction/resolve" client-out-chan)
+      (let [message {:jsonrpc "2.0"
+                     :id 101
+                     :method "codeAction/resolve"
+                     :params {:title "Foo" :kind "quickfix" :edit "some-file" :data {:alligator-source "s1"}}}]
+        (async/>!! client-out-chan message)
+
+        (let [relayed-message (async/<!! server1-in-chan)
+              expected-message (assoc-in message [:params :data] {})]
+          (is (= expected-message relayed-message)))))))
