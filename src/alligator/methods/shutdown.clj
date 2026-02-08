12a28cc6ee660139171f33169012992b1f03af7f
junyi.hou <junyi.yi.hou@gmail.com>
2026-02-08 00:58:45 -0800

    [bug fix] send shutdown message to client only after all servers shuts down

--- src/alligator/methods/shutdown.clj
+++ src/alligator/methods/shutdown.clj
@@ -7,15 +7,14 @@
 
 (defmethod  methods/process-server-message "shutdown"
   [_ input-chan output-chan]
-  (async/go-loop [shutdown-servers #{}]
+  (async/go-loop [shutdown-servers []]
     (if-let [{:keys [from message]} (async/<! input-chan)]
-      (do
-        ;; forward the shutdown response to the client
-        (async/>! output-chan message)
-        (let [new-shutdown-servers (conj shutdown-servers from)]
-          (if (>= (count new-shutdown-servers) (count @mux/enabled-servers))
-            ;; all servers have shutdown, close Alligator too
-            (async/close! mux/exit-chan)
-            (recur new-shutdown-servers))))
+      (let [new-shutdown-servers (conj shutdown-servers from)]
+        ;; only sends shutdown message when all servers replied
+        (if (>= (count new-shutdown-servers) (count @mux/enabled-servers))
+          (do
+            (async/>! output-chan message)
+            (async/close! mux/exit-chan))
+          (recur new-shutdown-servers)))
       ;; if channel is closed before we get all responses, still exit
       (async/close! mux/exit-chan))))
