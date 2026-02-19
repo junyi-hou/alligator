23b63bf89227924ef6709ddab768c332afed5b08
junyi.hou <junyi.yi.hou@gmail.com>
2026-02-18 22:55:22 -0800

    re-create multiservers mock test

---
+++ test/alligator/mock/multiservers/multiservers_test.clj
@@ -0,0 +1,27 @@
+(ns alligator.mock.multiservers.multiservers-test
+  (:require
+   [clojure.test :refer [deftest is testing use-fixtures]]
+   [alligator.methods :as methods]
+   [alligator.multiplexer :as mux]
+   [alligator.test-utils :as utils]))
+
+(use-fixtures :each utils/reset-all-states)
+
+(methods/load-handlers!)
+
+(deftest multi-server-integration-test
+  (let [{:keys [client]} (utils/start-servers-and-client
+                          ["--" "clj -M:test -m alligator.mock.basic.server" "--default"
+                           ;; alligator identifies servers using their executable, and two servers with the same name
+                           ;; will name collision
+                           "--" "clojure -M:test -m alligator.mock.basic.server" "-c" "completion-provider"])]
+
+    (testing "successfully launch 2 servers"
+      (is (= (count @mux/enabled-servers) 2)))
+
+    (testing "Handshake"
+      (let [resp (utils/request client "initialize" {:capabilities {} :root-uri "file:///"})]
+        (is (= "Alligator (clj+clojure)" (get-in resp [:result :server-info :name])))
+        (utils/notify client "initialized")))
+
+    (utils/shutdown-client client)))
